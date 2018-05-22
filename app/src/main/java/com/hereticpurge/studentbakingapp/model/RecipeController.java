1ad36ed1641a9984a340fae954b707e670f69e25
hereticpurge <hereticpurge@gmail.com>
2018-05-22 02:24:36 -0600

    Built out the RecipeBuilder inner class of RecipeController to handle Recipe object creation.

--- app/src/main/java/com/hereticpurge/studentbakingapp/model/RecipeController.java
+++ app/src/main/java/com/hereticpurge/studentbakingapp/model/RecipeController.java
@@ -3,6 +3,7 @@ package com.hereticpurge.studentbakingapp.model;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public final class RecipeController {
 
@@ -10,6 +11,18 @@ public final class RecipeController {
 
     private ArrayList<Recipe> mRecipeList;
     private static RecipeController sController;
+
+    //step map TAGs
+    public static final String STEP_ID = "id";
+    public static final String STEP_SHORT_DESCRIPTION = "shortDesc";
+    public static final String STEP_DESCRIPTION = "description";
+    public static final String STEP_VIDEO_URL = "videoUrl";
+    public static final String STEP_THUMBNAIL_URL = "thumbUrl";
+
+    //ingredient map TAGs
+    public static final String INGREDIENT_QUANTITY = "quantity";
+    public static final String INGREDIENT_MEASURE = "measure";
+    public static final String INGREDIENT = "ingredient";
 
     private RecipeController(){
         mRecipeList = new ArrayList<>();
@@ -31,10 +44,50 @@ public final class RecipeController {
 
     }
 
-    public class RecipeBuilder {
+    private class RecipeBuilder {
+        //the recipe being built
         Recipe mCurrentRecipe;
 
-        private RecipeBuilder(){}
+        //boolean to determine if this recipe has already been built and therefore shouldn't
+        // be changed.
+        private boolean mBuilt = false;
+
+        private RecipeBuilder(){
+            mCurrentRecipe = new Recipe();
+        }
+
+        public Recipe setId(String id){
+            mCurrentRecipe.setRecipeId(id);
+            return mCurrentRecipe;
+        }
+
+        public Recipe setTitle(String title){
+            mCurrentRecipe.setRecipeTitle(title);
+            return mCurrentRecipe;
+        }
+
+        public Recipe addStep(Map<String, String> step){
+            mCurrentRecipe.addStep(step.get(STEP_ID),
+                    step.get(STEP_SHORT_DESCRIPTION),
+                    step.get(STEP_DESCRIPTION),
+                    step.get(STEP_VIDEO_URL),
+                    step.get(STEP_THUMBNAIL_URL));
+            return mCurrentRecipe;
+        }
+
+        public Recipe addIngredient(Map<String, String> ingredient){
+            mCurrentRecipe.addIngredient(ingredient.get(INGREDIENT_QUANTITY),
+                    ingredient.get(INGREDIENT_MEASURE),
+                    ingredient.get(INGREDIENT));
+            return mCurrentRecipe;
+        }
+
+        public void build(){
+            if (!mBuilt) {
+                mRecipeList.add(mCurrentRecipe);
+            }
+            mBuilt = true;
+        }
 
     }
 
